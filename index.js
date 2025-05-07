a6e37b504b3baf33339e807bab6d0556cf1cb835
vedozer <vedozer@gmail.com>
2025-05-07 19:51:44 +0300

    cikarma,carpma,bolme fonksiyonları hazırlandı

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const toplama = (a,b) => {
     return a+b;
 }
+
+const cikarma = (a,b) => {
+    return a-b;
+}
+
+const carpma = (a,b) => {
+    return a*b;
+}
+
+const bolme = (a,b) => {
+    return a/b;
+}
